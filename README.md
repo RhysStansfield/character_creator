bb09062cecebe85ae990eae3266e126f63f8d0b1
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2013-10-12 03:58:07 +0100

    Initial commit - README created and first attempt at name and age creator (name_age.rb).

---
+++ README.md
@@ -0,0 +1,6 @@
+Basic command line based character creation program.
+
+Currently consists of the following files:
+
+        *README.md
+        *name_age.rb
---
+++ name_age.rb
@@ -0,0 +1,58 @@
+class PersonCreator
+
+  def prompt
+    '  >    '
+  end
+
+  def user_input
+    print prompt
+    get_user_input = gets.chomp
+  end
+
+  def gets_name
+    puts 'Okay cool!  What\'s your name then?'
+    name = user_input
+    check_name(name)
+  end
+
+  def gets_age
+    puts 'Awesome, that\'s your name sorted - what is your age?'
+    age = user_input
+    check_age(age)
+  end
+
+  def check_name(name)
+    puts "So your name is #{name}, yes?"
+    name_check = user_input.downcase
+    return name_fail if name_check == 'no'
+    gets_age
+  end
+
+  def check_age(age)
+    puts "Okay, your age is #{age}?"
+    age_check = user_input.downcase
+    return age_fail if age_check == 'no'
+    puts success
+  end
+
+  def name_fail
+    puts 'Oh dear, lets sort that out, please enter your name:'
+    new_name = user_input
+    check_name(new_name)
+  end
+
+  def age_fail
+    puts 'Ooopsy, lets fix that, please enter it again:'
+    new_age = user_input
+    check_age(new_age)
+  end
+
+  def success
+    'Great!  Glad to know that\'s all sorted!'
+  end
+
+end
+
+person = PersonCreator.new
+
+person.gets_name
